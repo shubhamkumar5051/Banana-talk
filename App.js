b7736700613590ce8ef63e70a386fc52ff651da7
Shubham kumar <shubhamkr5051@gmail.com>
2021-07-20 16:00:36 +0530

    mock server API testing done

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 var btnRefer = document.querySelector("#btnClick");
 var txtInput = document.querySelector("#textStory");
 var outputDiv = document.querySelector("#output");
+var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranslationURL(text) {
+  return serverUrl + "?" + "text=" + text;
+}
 
 function clickHandler() {
-  outputDiv.innerHTML = "Translated : dfhgkjdfhkghdkl " + txtInput.value;
-};
+  // outputDiv.innerHTML = "Translated : dfhgkjdfhkghdkl " + txtInput.value;
+
+  var inputText = txtInput.value; //taking input
+  fetch(getTranslationURL(inputText)) //calling server for processing
+    .then((response) => response.json())
+    .then((json) => console.log(json.contents.translated));
+}
 
 btnRefer.addEventListener("click", clickHandler);
