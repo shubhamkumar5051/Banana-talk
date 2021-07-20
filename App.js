b9b81a9d4f1ef7324ece5515ffecea806103b851
Shubham kumar <shubhamkr5051@gmail.com>
2021-07-20 16:34:43 +0530

    replace mock url and wired fetch call

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 var btnRefer = document.querySelector("#btnClick");
 var txtInput = document.querySelector("#textStory");
 var outputDiv = document.querySelector("#output");
-var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverUrl = "https://api.funtranslations.com/translate/chef.json";
 
 function getTranslationURL(text) {
   return serverUrl + "?" + "text=" + text;
 }
 
 function errorHandler(error) {
-  console.log("Error occured at server side ", error)
-  alert("Server down, Try Again Later")
+  console.log("Error occured at server side ", error);
+  alert("Server down, Try Again Later");
 }
 
 function clickHandler() {
@@ -18,8 +18,11 @@ function clickHandler() {
   var inputText = txtInput.value; //taking input
   fetch(getTranslationURL(inputText)) //calling server for processing
     .then((response) => response.json())
-    .then((json) => console.log(json.contents.translated))
-    .catch(errorHandler)
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler); //we dont have to pass error argument to errorHandler function
 }
 
 btnRefer.addEventListener("click", clickHandler);
